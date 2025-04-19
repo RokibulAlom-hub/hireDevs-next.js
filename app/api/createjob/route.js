919659eaf09c126f906228ec0f3b3d484d4852c0
Rokib Shuvo <itismehey975@gmail.com>
2025-04-19 13:04:31 +0600

    added the search operations successfully user can search with any keyword

--- app/api/createjob/route.js
+++ app/api/createjob/route.js
@@ -14,20 +14,28 @@ export const GET = async (req) => {
   await connectDB();
   const url = new URL(req.url); // makes a full URL object
   const searchParams = url.searchParams;
-  // const {searchParams} = new URL(req.url); this is short cut of uper two line 
-  console.log(searchParams);
+  // const {searchParams} = new URL(req.url); this is short cut of uper two line ff
+  // console.log(searchParams);
 
   // searchParams gives you easy access to all query params using .get("paramName").
   const type = searchParams.get("type");
   const location = searchParams.get("location");
   const title = searchParams.get("title");
-
+  const search  =searchParams.get("search")
   // this logic set the value of filter conditionally
   let filter = {};
   if (type) filter.type = type;
   if (location) filter.location = location;
   if (title) filter.title = title;
 
+  //  this logic is for search query 
+  if(search){
+    filter.$or = [
+      {title:{$regex: search , $options:"i"}},
+      {location:{$regex: search , $options:"i"}},
+      {type:{$regex: search , $options:"i"}},
+    ]
+  }
   const result = await CreateJob.find(filter);
   return NextResponse.json(result);
 };
--- app/jobs/page.jsx
+++ app/jobs/page.jsx
@@ -2,7 +2,7 @@ import CardParent from "../Components/Card";
 
 export default async function Jobs({ searchParams }) {
   const query = new URLSearchParams(searchParams).toString()
-  console.log("this is searchParams", searchParams,"this is query", query);
+  // console.log("this is searchParams", searchParams,"this is query", query);
   
   // const query = type ? `?type=${type}` : "";
   const res = await fetch(`http://localhost:3000/api/createjob?${query}`, {
@@ -14,16 +14,31 @@ export default async function Jobs({ searchParams }) {
     <div className="p-6">
       {/* Filter Buttons that reload the page with new query */}
       <div className="flex gap-4 mb-6">
+        <a href="/jobs" className="btn">
+          All
+        </a>
         <a href="/jobs?type=Full-time" className="btn">
           Full-time
         </a>
         <a href="/jobs?type=Internship" className="btn">
           Internship
         </a>
-        <a href="/jobs" className="btn">
-          All
+        <a href="/jobs?location=Remote" className="btn">
+         Remote
         </a>
+       
+      
       </div>
+      {/* this form is search query */}
+
+      <form method="get">
+        <input type="text"
+        name="search"
+        placeholder="search by keyword"
+        className=""
+         />
+         <button type="submit">Search</button>  
+      </form>
       <CardParent jobs={jobs}></CardParent>
     </div>
   );
